a3a5bb66480aeecbdfec49d8302751d22a5575ea
Tiramister <nishi31416@docomo.ne.jp>
2018-11-04 20:12:35 +0900

    DDCC2017qual-D AC

--- others/DDCC/2017/qual/D.cpp
+++ others/DDCC/2017/qual/D.cpp
@@ -1,149 +1,67 @@
-/* main code starts from line 130. */
-
-/* ---------- STL Libraries ---------- */
-
-// IO library
-#include <cstdio>
-#include <iomanip>
-#include <ios>
 #include <iostream>
-
-// algorithm library
-#include <algorithm>
-#include <cmath>
-#include <numeric>
-
-// container library
-#include <bitset>
-#include <map>
-#include <queue>
-#include <set>
 #include <string>
-#include <vector>
-
-/* ---------- Namespace ---------- */
 
 using namespace std;
 
-/* ---------- Type Abbreviation ---------- */
-
-template <typename T>
-using V = vector<T>;
-template <typename T, typename U>
-using P = pair<T, U>;
-template <typename T>
-using PQ = priority_queue<T>;
-
-using ll = long long;
-using str = string;
-
-#define fst first
-#define snd second
-#define pb push_back
-#define mp make_pair
-#define mt make_tuple
-
-/* ---------- conversion ---------- */
-
-#define INT(c) static_cast<int>(c)
-#define CHAR(n) static_cast<char>(n)
-#define LL(n) static_cast<ll>(n)
-#define DOUBLE(n) static_cast<double>(n)
-
-/* ---------- container ---------- */
-
-#define ALL(v) (v).begin(), (v).end()
-#define SIZE(v) (LL((v).size()))
-
-#define FIND(v, k) (v).find(k) != (v).end()
-#define VFIND(v, k) find(ALL(v), k) != (v).end
-
-#define SORT(v) sort(ALL(v))
-#define GSORT(v) sort(ALL(v), greater<decltype((v).front())>())
-
-/* ---------- repetition ---------- */
-
-#define FOR(i, a, b) for (ll i = (a); i < (b); i++)
-#define REP(i, n) FOR(i, 0, n)
-#define NREP(i, n) FOR(i, 1, n + 1)
-
-#define RFOR(i, a, b) for (ll i = (a); i >= (b); i--)
-#define RREP(i, n) RFOR(i, n - 1, 0)
-#define RNREP(i, n) RFOR(i, n, 1)
-
-// Usual REP runs from 0 to n-1 (R: n-1 to 0)
-// Natural REP runs from 1 to n (R: n to 1)
-
-/* ---------- Short Functions ---------- */
-
-template <typename T>
-T sq(T a) {
-    return a * a;
-}
-
-#define fcout cout << fixed << setprecision(10)
-
-/* ----------- debug ---------- */
-
-template <typename T, typename U>
-void testP2(T a, U b) {
-    cout << "(" << a << ", " << b << ")" << endl;
-    return;
-}
-
-template <typename T>
-void testV(T v) {
-    cout << "[";
-    for (auto i : v) {
-        cout << i << ", ";
-    }
-    cout << "\b\b]" << endl;
-    return;
-}
-
-template <typename T>
-void testV2(T v) {
-    for (auto sv : v) {
-        testV(sv);
-    }
-    cout << endl;
-    return;
-}
-
-#define GET_VAR_NAME(variable) #variable
-#define test(x) cout << GET_VAR_NAME(x) << " = " << x << endl;
-#define testP(p)                      \
-    cout << GET_VAR_NAME(p) << " = "; \
-    testP2(p.fst, p.snd);
-
-/* ---------- Constants ---------- */
-
-// const ll MOD = 1e9 + 7;
-// const int INF = 1 << 25;
-// const ll INF = 1LL << 50;
-// const double PI = 3.14159265358979;
-// const ll dx[4] = {0, -1, 1, 0};
-// const ll dy[4] = {-1, 0, 0, 1};
-// const ll dx[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
-// const ll dy[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
-
-/* v-v-v-v-v-v-v-v-v Main Part v-v-v-v-v-v-v-v-v */
-
-/* ---------- Type Definition ----------- */
-
-
-/* ---------- Global Variance ----------- */
-
-
-/* ------------- Functions -------------- */
-
-
-/* ----------- Main Function ------------ */
-
 int main() {
-    cin.tie(0);
-    ios::sync_with_stdio(false);
+    int H, W, A, B;
+    cin >> H >> W >> A >> B;
 
+    bool m[H][W];
+    for (int x = 0; x < H; ++x) {
+        string S;
+        cin >> S;
+        for (int y = 0; y < W; ++y) {
+            m[x][y] = (S[y] == 'S');
+        }
+    }
 
+    // 初期状態における対称性を求める
+    bool ajudge = true, bjudge = true;
+    for (int x = 0; x < H; ++x) {
+        for (int y = 0; y < W; ++y) {
+            if (m[x][y] != m[H - x - 1][y]) ajudge = false;
+            if (m[x][y] != m[x][W - y - 1]) bjudge = false;
+        }
+    }
+
+    int ans = 0;
+    if (!ajudge) ans += A;
+    if (!bjudge) ans += B;
+
+    // ゴミを撤去する
+    for (int x = 0; x < H; ++x) {
+        for (int y = 0; y < W; ++y) {
+            if (!m[H - x - 1][y] && !m[x][W - y - 1]) m[x][y] = false;
+        }
+    }
+
+    // 上下左右対称なものをスコアに加算して取り除く
+    for (int x = 0; x < H / 2; ++x) {
+        for (int y = 0; y < W / 2; ++y) {
+            if (m[x][y] && m[H - x - 1][y] &&
+                m[x][W - y - 1] && m[H - x - 1][W - y - 1]) {
+                ans += A + B + max(A, B);
+                m[x][y] = m[H - x - 1][y] =
+                    m[x][W - y - 1] = m[H - x - 1][W - y - 1] = false;
+            }
+        }
+    }
+
+    // 上下対称、左右対称な組をそれぞれ数える
+    // 重複して数えていることに注意
+    int a = 0, b = 0;
+    for (int x = 0; x < H; ++x) {
+        for (int y = 0; y < W; ++y) {
+            a += (m[x][y] && m[H - x - 1][y]);
+            b += (m[x][y] && m[x][W - y - 1]);
+        }
+    }
+    a /= 2, b /= 2;
+
+    // そのうちよりスコアが稼げる方のみを加算
+    ans += max(A * a, B * b);
+
+    cout << ans << endl;
     return 0;
 }
