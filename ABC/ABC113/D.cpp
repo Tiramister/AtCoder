a44026441d0171b6e8a4757a8a9acc03de8853b5
Tiramister <nishi31416@docomo.ne.jp>
2018-11-08 06:13:12 +0900

    ABC113-D 行列累乗版解法

--- ABC/ABC113/D.cpp
+++ ABC/ABC113/D.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -6,48 +7,84 @@ using ll = long long;
 
 const ll MOD = 1e9 + 7;
 
-int main() {
-    int H, W, K;
-    cin >> H >> W >> K;
+ll fib_t[1000];
 
-    --K;
-    // 以降縦棒の番号は0-indexedで扱う
+ll fib(int n) {
+    if (n < 0) return 1;
+    if (fib_t[n] > 0) return fib_t[n];
+    return fib_t[n] = (fib(n - 1) + fib(n - 2)) % MOD;
+}
 
-    ll dp[H + 1][W];
-    fill(dp[0], dp[H + 1], 0);
-    dp[0][0] = 1;
-    // dp[h][k] = 横線h段からなるあみだくじで、0がkに来るものの数
+// 行列の積(正方行列を仮定)
+vector<vector<ll>> matmul(vector<vector<ll>> a, vector<vector<ll>> b) {
+    int l = a[0].size();
 
-    for (int h = 0; h < H; ++h) {
-        for (int b = 0; b < (1 << (W - 1)); ++b) {
+    vector<vector<ll>> ret(l, vector<ll>(l, 0));
 
-            // 横線が隣り合っていないかチェック
-            bool judge = true;
-            for (int i = 0; i < W - 2; ++i) {
-                // これで末尾が11のときに弾くことができる
-                if ((b >> i) % 4 == 3) judge = false;
-            }
-
-            if (!judge) continue;
-
-            // 各始点がどこへ移動するか求める
-            int perm[W];
-            for (int i = 0; i < W; ++i)
-                perm[i] = i;
-
-            for (int i = 0; i < W - 1; ++i) {
-                // あみだくじのルールに従って位置をswap
-                if ((b >> i) & 1) swap(perm[i], perm[i + 1]);
-            }
-
-            // 配る
-            for (int i = 0; i < W; ++i) {
-                dp[h + 1][perm[i]] += dp[h][i];
-                dp[h + 1][perm[i]] %= MOD;
+    for (int i = 0; i < l; ++i) {
+        for (int j = 0; j < l; ++j) {
+            for (int k = 0; k < l; ++k) {
+                ret[i][j] += a[i][k] * b[k][j];
+                ret[i][j] %= MOD;
             }
         }
     }
 
-    cout << dp[H][K] << endl;
+    return ret;
+}
+
+// ダブリングによる行列累乗
+vector<vector<ll>> matpow(vector<vector<ll>> m, int n) {
+    int l = m[0].size();
+
+    // 単位行列を返す
+    if (n == 0) {
+        for (int i = 0; i < l; ++i) {
+            for (int j = 0; j < l; ++j) {
+                m[i][j] = (i == j);
+            }
+        }
+        return m;
+    }
+
+    if (n == 1) return m;
+
+    if (n % 2) {
+        return matmul(matpow(m, n - 1), m);
+    } else {
+        auto half = matpow(m, n / 2);
+        return matmul(half, half);
+    }
+}
+
+int main() {
+    fib_t[0] = 1, fib_t[1] = 1;
+
+    int H, W, K;
+    cin >> H >> W >> K;
+    --K;
+    // 以降縦棒の番号は0-indexedで扱う
+
+    vector<vector<ll>> pat(W, vector<ll>(W, 0));
+
+    // 左に移動するパターン
+    for (int i = 1; i < W; ++i) {
+        pat[i - 1][i] = fib(i - 1) * fib(W - i - 1) % MOD;
+    }
+
+    // 右に移動するパターン
+    for (int i = 0; i < W - 1; ++i) {
+        pat[i + 1][i] = fib(i) * fib(W - i - 2) % MOD;
+    }
+
+    // 真下に移動するパターン
+    for (int i = 0; i < W; ++i) {
+        pat[i][i] = fib(i) * fib(W - i - 1) % MOD;
+    }
+
+    auto res = matpow(pat, H);
+
+    cout << res[K][0] << endl;
+
     return 0;
 }
