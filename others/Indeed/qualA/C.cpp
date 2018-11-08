c527c856bfa1fbfa2e63c913cc701c625410e347
Tiramister <nishi31416@docomo.ne.jp>
2018-11-08 19:54:59 +0900

    Indeed qualA - C editorial解法

--- others/Indeed/qualA/C.cpp
+++ others/Indeed/qualA/C.cpp
@@ -1,45 +1,27 @@
+#include <algorithm>
 #include <iostream>
 
 using namespace std;
-
-const int MAX_V = 1000000;
-int cnt[MAX_V + 10];
 
 int main() {
     int N;
     cin >> N;
 
+    int s[N + 1];
     for (int i = 0; i < N; ++i) {
-        int s;
-        cin >> s;
-        ++cnt[s];
+        cin >> s[i];
     }
-
-    for (int i = 1; i <= MAX_V; ++i) {
-        cnt[i] += cnt[i - 1];
-    }
+    s[N] = 0;
+    sort(s, s + N, greater<int>());
 
     int Q;
     cin >> Q;
+
     for (int q = 0; q < Q; ++q) {
         int k;
         cin >> k;
-        if (k >= N - cnt[0]) {
-            cout << 0 << endl;
-            continue;
-        }
-
-        int ok = MAX_V + 1, ng = 0;
-        while (ok - ng > 1) {
-            int mid = (ok + ng) / 2;
-            if (N - cnt[mid - 1] <= k) {
-                ok = mid;
-            } else {
-                ng = mid;
-            }
-        }
-
-        cout << ok << endl;
+        cout << s[k] + (s[k] > 0) << endl;
     }
+
     return 0;
 }
