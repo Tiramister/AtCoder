db8329372f018013f52bb5cda2db2097b65f15e5
Tiramister <nishi31416@docomo.ne.jp>
2018-11-30 07:01:17 +0900

    ABC001-D 整形

--- ABC/ABC001/D.cpp
+++ ABC/ABC001/D.cpp
@@ -1,166 +1,51 @@
-/* main code starts from line 123. */
-
-/* ---------- STL Libraries ---------- */
-
-// IO library
-#include <cstdio>
-#include <iomanip>
-#include <ios>
 #include <iostream>
-
-// algorithm library
-#include <algorithm>
-#include <cmath>
-#include <numeric>
-
-// container library
-#include <deque>
-#include <list>
-#include <map>
-#include <queue>
-#include <set>
 #include <string>
-#include <vector>
-
-
-/* ---------- Namespace ---------- */
 
 using namespace std;
 
-
-/* ---------- Type Abbreviation ---------- */
-
-template <typename T>
-using V = vector<T>;
-template <typename T, typename U>
-using P = pair<T, U>;
-
-using ll = long long;
-using PL = P<ll, ll>;
-using PS = P<string, ll>;
-
-using VI = V<int>;
-using VVI = V<VI>;
-using VL = V<ll>;
-using VVL = V<VL>;
-using VS = V<string>;
-using VB = V<bool>;
-using VVB = V<VB>;
-using VPL = V<PL>;
-using VPS = V<PS>;
-
-#define fst first
-#define snd second
-#define pb push_back
-
-
-/* ---------- conversion ---------- */
-
-#define INT(c) static_cast<int>(c)
-#define CHAR(n) static_cast<char>(n)
-#define LL(n) static_cast<ll>(n)
-#define DOUBLE(n) static_cast<double>(n)
-
-
-/* ---------- container ---------- */
-
-#define ALL(v) (v).begin(), (v).end()
-#define SIZE(v) (LL((v).size()))
-
-#define FIND(v, k) (v).find(k) != (v).end()
-#define EACH(itr, v) for (auto itr = (v).begin(); itr != (v).end(); itr++)
-#define REACH(i, v) for (auto itr = (v).rbegin(); itr != (v).rend(); itr++)
-
-#define SORT(v) sort(ALL(v))
-#define GSORT(v) sort(ALL(v), greater<decltype((v).front())>())
-
-
-/* ---------- repetition ---------- */
-
-#define FOR(i, a, b) for (ll i = (a); i < (b); i++)
-#define REP(i, n) FOR(i, 0, n)
-#define NREP(i, n) FOR(i, 1, n + 1)
-
-#define RFOR(i, a, b) for (ll i = (a); i >= (b); i--)
-#define RREP(i, n) RFOR(i, n - 1, 0)
-#define RNREP(i, n) RFOR(i, n, 1)
-
-// Usual REP runs from 0 to n-1 (R: n-1 to 0)
-// Natural REP runs from 1 to n (R: n to 1)
-
-
-/* ---------- Constants ---------- */
-
-// const ll MOD = 1e9 + 7;
-// const int INF = 1 << 25;
-// const ll INF = 1LL << 50;
-// const double PI = 3.14159265358979;
-// const ll dx[4] = {0, -1, 1, 0};
-// const ll dy[4] = {-1, 0, 0, 1};
-// const ll dx[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
-// const ll dy[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
-
-
-/* ---------- Short Functions ---------- */
-
-template <typename T>
-T sq(T a) {
-    return a * a;
+int s2i(string S) {
+    int h = stoi(S.substr(0, 2));
+    int m = stoi(S.substr(2, 2));
+    return h * 60 + m;
 }
 
+string i2s(int t) {
+    string h = to_string(t / 60);
+    if (h.size() < 2) h = "0" + h;
 
-/* ---------- Debug ---------- */
+    string m = to_string(t % 60);
+    if (m.size() < 2) m = "0" + m;
 
-#define GET_VAR_NAME(variable) #variable
-
-#define test(x) cout << GET_VAR_NAME(x) << " = " << x << endl;
-#define testP(p) cout << GET_VAR_NAME(p) << " = (" << (p).fst << ", " << (p).snd << ")" << endl;
-#define testP2(a, b) cout << "(" << a << ", " << b << ")" << endl;
-
-
-/* v-v-v-v-v-v-v-v-v Main Part v-v-v-v-v-v-v-v-v */
-
-/* ---------- Global Variance ----------- */
-
-
-/* ------------- Functions -------------- */
-
-
-/* ----------- Main Function ------------ */
+    return h + m;
+}
 
 int main() {
-    cin.tie(0);
-    ios::sync_with_stdio(false);
-
     int N;
     cin >> N;
-    VI imos(300, 0);
-    REP(i, N) {
+
+    int imos[2000];
+    fill(imos, imos + 2000, 0);
+
+    for (int i = 0; i < N; ++i) {
         string S;
         cin >> S;
-        int from = stoi(S.substr(0, 4));
-        int to = stoi(S.substr(5, 4));
-        int hfrom = from / 100;
-        int mfrom = from % 100 / 5;
-        int hto = to / 100;
-        int mto = (to % 100 > 0 ? (to % 100 - 1) / 5 + 1 : 0);
-        imos[hfrom * 12 + mfrom]++;
-        imos[hto * 12 + mto]--;
+        int b = s2i(S.substr(0, 4));
+        int e = s2i(S.substr(5, 4));
+        ++imos[b / 5 * 5];
+        --imos[(e + 4) / 5 * 5];
+    }
+
+    for (int t = 1; t < 2000; ++t) {
+        imos[t] += imos[t - 1];
     }
 
     bool rain = false;
-    int sum = 0;
-    REP(i, 300) {
-        sum += imos[i];
-        if (!rain && sum > 0) {
-            cout << setw(2) << setfill('0') << i / 12;
-            cout << setw(2) << setfill('0') << i % 12 * 5;
-            cout << "-";
+    for (int t = 0; t < 2000; ++t) {
+        if (!rain && imos[t] > 0) {
+            cout << i2s(t) << "-";
             rain = true;
-        } else if (rain && sum == 0) {
-            cout << setw(2) << setfill('0') << i / 12;
-            cout << setw(2) << setfill('0') << i % 12 * 5;
-            cout << endl;
+        } else if (rain && imos[t] == 0) {
+            cout << i2s(t) << endl;
             rain = false;
         }
     }
