e126eaef73c95bb64b07eaeadfcf4fdedf993517
Tiramister <nishi31416@docomo.ne.jp>
2019-02-26 19:50:37 +0900

    ARC025-C ReAC

--- ARC/ARC025/C.cpp
+++ ARC/ARC025/C.cpp
@@ -1,185 +1,69 @@
-/* main code starts from line 130. */
-
-/* ---------- STL Libraries ---------- */
-
-// IO library
-#include <cstdio>
-#include <iomanip>
-#include <ios>
 #include <iostream>
-
-// algorithm library
 #include <algorithm>
-#include <cmath>
-#include <numeric>
-
-// container library
-#include <bitset>
-#include <map>
 #include <queue>
-#include <set>
-#include <string>
+#include <tuple>
 #include <vector>
 
-/* ---------- Namespace ---------- */
-
 using namespace std;
-
-/* ---------- Type Abbreviation ---------- */
-
-template <typename T>
-using V = vector<T>;
-template <typename T, typename U>
-using P = pair<T, U>;
-template <typename T>
-using PQ = priority_queue<T>;
-
 using ll = long long;
-using str = string;
 
-#define fst first
-#define snd second
-#define pb push_back
-#define mp make_pair
-#define mt make_tuple
-
-/* ---------- conversion ---------- */
-
-#define INT(c) static_cast<int>(c)
-#define CHAR(n) static_cast<char>(n)
-#define LL(n) static_cast<ll>(n)
-#define DOUBLE(n) static_cast<double>(n)
-
-/* ---------- container ---------- */
-
-#define ALL(v) (v).begin(), (v).end()
-#define SIZE(v) (LL((v).size()))
-
-#define FIND(v, k) (v).find(k) != (v).end()
-#define VFIND(v, k) find(ALL(v), k) != (v).end
-
-#define SORT(v) sort(ALL(v))
-#define GSORT(v) sort(ALL(v), greater<decltype((v).front())>())
-
-/* ---------- repetition ---------- */
-
-#define FOR(i, a, b) for (ll i = (a); i < (b); i++)
-#define REP(i, n) FOR(i, 0, n)
-#define NREP(i, n) FOR(i, 1, n + 1)
-
-#define RFOR(i, a, b) for (ll i = (a); i >= (b); i--)
-#define RREP(i, n) RFOR(i, n - 1, 0)
-#define RNREP(i, n) RFOR(i, n, 1)
-
-// Usual REP runs from 0 to n-1 (R: n-1 to 0)
-// Natural REP runs from 1 to n (R: n to 1)
-
-/* ---------- Short Functions ---------- */
-
-template <typename T>
-T sq(T a) {
-    return a * a;
-}
-
-#define fcout cout << fixed << setprecision(10)
-
-/* ----------- debug ---------- */
-
-template <typename T, typename U>
-void testP2(T a, U b) {
-    cout << "(" << a << ", " << b << ")" << endl;
-    return;
-}
-
-template <typename T>
-void testV(T v) {
-    cout << "[";
-    for (auto i : v) {
-        cout << i << ", ";
-    }
-    cout << "\b\b]" << endl;
-    return;
-}
-
-template <typename T>
-void testV2(T v) {
-    for (auto sv : v) {
-        testV(sv);
-    }
-    cout << endl;
-    return;
-}
-
-#define GET_VAR_NAME(variable) #variable
-#define test(x) cout << GET_VAR_NAME(x) << " = " << x << endl;
-#define testP(p)                      \
-    cout << GET_VAR_NAME(p) << " = "; \
-    testP2(p.fst, p.snd);
-
-/* ---------- Constants ---------- */
-
-// const ll MOD = 1e9 + 7;
-// const int INF = 1 << 25;
-const ll INF = 1LL << 50;
-// const double PI = 3.14159265358979;
-// const ll dx[4] = {0, -1, 1, 0};
-// const ll dy[4] = {-1, 0, 0, 1};
-// const ll dx[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
-// const ll dy[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
-
-/* v-v-v-v-v-v-v-v-v Main Part v-v-v-v-v-v-v-v-v */
-
-/* ---------- Type Definition ----------- */
-
-
-/* ---------- Global Variance ----------- */
-
-
-/* ------------- Functions -------------- */
-
-
-/* ----------- Main Function ------------ */
+const int INF = 1 << 30;
 
 int main() {
-    cin.tie(0);
-    ios::sync_with_stdio(false);
-
-    ll N, M, R, T;
+    int N, M, R, T;
     cin >> N >> M >> R >> T;
-    V<P<ll, ll>> path(N + 1);
-    REP(i, M) {
-        ll a, b, c;
+
+    vector<pair<int, int>> path[N];
+    for (int i = 0; i < M; ++i) {
+        int a, b, c;
         cin >> a >> b >> c;
-        path[a].pb(mp(b, c));
-        path[b].pb(mp(a, c));
+        --a, --b;
+        path[a].push_back(make_pair(b, c));
+        path[b].push_back(make_pair(a, c));
     }
 
-    NREP(A, N) {
-        PQ<P<ll, ll>> q;
-        V<ll> d(N + 1, INF);
-        d[A] = 0;
-        q.push(mp(0, A));
-        while (!q.empty()) {
-            auto p = q.top();
-            q.pop();
+    ll ans = 0;
+    for (int A = 0; A < N; ++A) {
+        int dist[N];
+        fill(dist, dist + N, INF);
+        dist[A] = 0;
 
-            c = p.fst;
-            v = p.snd;
-            if (d[v] < INF) continue;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> que;
+        que.push(make_pair(0, A));
 
-            d[v] = c;
-            for (auto sp : path[v]) {
-                sv = sp.fst;
-                sc = sp.snd;
-                que.push(mp(sc + c, sv));
+        while (!que.empty()) {
+            int d, v;
+            tie(d, v) = que.top();
+            que.pop();
+            if (d > dist[v]) continue;
+
+            for (auto p : path[v]) {
+                int sv, c;
+                tie(sv, c) = p;
+                if (dist[sv] <= dist[v] + c) continue;
+                dist[sv] = dist[v] + c;
+                que.push(make_pair(dist[sv], sv));
             }
         }
 
-        SORT(d);
-        ll ti = 1, ri = 2;
-        while (ri < N) {
-            if (d[ri])
+        sort(dist, dist + N);
+        for (int B = 1; B < N; ++B) {
+            int ok = 0, ng = N;
+            // dist[ok] / T < dist[B] / R
+
+            while (ng - ok > 1) {
+                int mid = (ok + ng) / 2;
+                if (dist[mid] * R < dist[B] * T) {
+                    ok = mid;
+                } else {
+                    ng = mid;
+                }
+            }
+            ans += ok;
+            if (ok >= B) --ans;
         }
     }
+
+    cout << ans << endl;
     return 0;
 }
